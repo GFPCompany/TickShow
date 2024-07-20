2367d27b4a048169b41bfba421bda8720c8cf1ba
souhhh <bulkin-2023@internet.ru>
2024-07-20 23:01:18 +0300

    update count component
    update Count component

--- src/Count/ui/Count.jsx
+++ src/Count/ui/Count.jsx
@@ -1,9 +1,8 @@
-// import { useState } from "react"
 import minus from './assets/Minus.svg'
 import plus from './assets/Plus.svg'
 import styles from './Count.module.scss'
 
-const Count = () => {
+const Count = ({ value, onChange }) => {
 
     // const [count,setCount] = useState(0) 
     
@@ -19,15 +18,15 @@ const Count = () => {
             <div className={styles.display}>
 
                 <div>
-                    <button className={styles.buttonMinus}><img src={minus} alt="" /></button>
+                    <button className={styles.buttonMinus} onClick={() => onChange(value - 1)}><img src={minus} alt="" /></button>
                 </div>
 
                 <div>
-                    <input type="text"/>
+                    <input type="number" value={value} onChange={event => onChange(event.target.value)}/>
                 </div>
 
                 <div>
-                    <button className={styles.buttonPlus}><img src={plus} alt="" /></button>
+                    <button className={styles.buttonPlus} onClick={() => onChange(value + 1)}><img src={plus} alt="" /></button>
                 </div>
             </div>
         </div>
